2895b1520b5976987a6f26687536991adaaf2514
putuvandi <pm.putuvandi@gmail.com>
2023-06-05 16:45:51 +0800

    Episode 14: View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -64,3 +66,6 @@ Route::get('/authors/{author:username}', function(User $author) {
         'posts' => $author->posts->load('category', 'author'),
     ]); 
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
