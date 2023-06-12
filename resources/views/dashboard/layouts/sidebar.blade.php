4cd3b6337ef8693135823415038c6728acca172e
putuvandi <pm.putuvandi@gmail.com>
2023-06-12 15:29:47 +0800

    Episode 23: Authorization

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -19,6 +19,20 @@
             </a>
           </li>
         </ul>
+        
+        @can('admin')
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Administrator</span>
+        </h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+              <i class="bi bi-grid"></i>
+            Post Categories
+            </a>
+          </li>
+        </ul>
+        @endcan
       </div>
     </div>
   </div>
